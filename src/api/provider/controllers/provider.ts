279888acc4ab9c35920e3254fe66adff8612aeaf
Lester Gime <85656635+lester24-dev@users.noreply.github.com>
2026-06-09 18:15:09 +0800

    Add files via upload

---
+++ src/api/provider/controllers/provider.ts
@@ -0,0 +1,41 @@
+/**
+ * provider controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::provider.provider', ({strapi}) => ({
+    async find(ctx) {
+     // Get query params (with defaults)
+    const page = parseInt((ctx.query.page as string) || '1', 10);
+    const pageSize = parseInt((ctx.query.pageSize as string) || '10', 10);
+    const filters = (ctx.query.filters ?? {}) as Record<string, any>;
+    // Merge filters (ensure deleted_at IS NULL)
+    ctx.query = {
+      ...ctx.query,
+      filters: {
+        ...filters,
+        deleted_at: { $null: true },
+        statuses: ctx.query.status,
+      },
+      pagination: {
+        page,
+        pageSize,
+      },
+    };
+
+     const { data, meta } = await super.find(ctx);
+
+        return {
+        data,
+        meta: {
+            pagination: {
+            page,
+            pageSize,
+            pageCount: meta.pagination.pageCount || 1,
+            total: meta.pagination.total || data.length,
+            },
+        },
+        };
+    },
+}));
